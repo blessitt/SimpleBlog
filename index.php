a58dbec012981c9a68ead74747c1ff7f536b58f1
Carter Smith Craney <carter.craneyhgs.hiddengeniusproject.org@Carters-MacBook-Pro.local>
2021-03-02 18:13:11 -0800

    added new header.php file

--- index.php
+++ index.php
@@ -19,31 +19,13 @@
   <title>Blog</title>
 </head>
 
-<body>
-  <header>
-    <div class="logo">
-      <h1 class="logo-text"><span>Carter</span>Smith</h1>
-    </div>
-    <i class="fa fa-bars menu-toggle"></i>
-    <ul class="nav">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <!-- <li><a href="#">Sign Up</a></li>
-      <li><a href="#">Login</a></li> -->
-      <li>
-        <a href="#">
-          <i class="fa fa-user"></i>
-          Carter Smith
-          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
-        </a>
-        <ul>
-          <li><a href="#">Dashboard</a></li>
-          <li><a href="#" class="logout">Logout</a></li>
-        </ul>
-      </li>
-    </ul>
-  </header>
+ 
+
+
+  <body> 
+
+  <?php include("app/includes/header.php"); ?>
+
 
   <!-- Page Wrapper -->
   <div class="page-wrapper">
