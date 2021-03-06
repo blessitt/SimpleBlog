f36110d9e7ec3a70ac7c6ce59fa1dff8906bce3e
Carter Smith <carter.craney@hgs.hiddengeniusproject.org>
2021-03-05 18:45:29 -0800

    added new code to the db.php

--- app/database/db.php
+++ app/database/db.php
@@ -3,9 +3,35 @@
 require('connect.php'); 
 
 
-$sql = "SELECT * FROM users"; 
-$stmt = $conn->prepare($sql); 
-$stmt->execute(); 
-$users = $stmt->get_result()->fetch_all(MYSQLI_ASSOC); 
 
-var_dump($users);
+function dd($value) // to be deleted
+{ 
+    echo "<pre>", print_r($value, true), "</pre>"; 
+    die();
+}
+
+
+
+function selectAll($table, $conditions = []) 
+{ 
+    global $conn; 
+    $sql = "SELECT * FROM $table"; 
+    if (empty($conditions)) {
+    $stmt = $conn->prepare($sql); 
+    $stmt->execute(); 
+    $records = $stmt->get_result()->fetch_all(MYSQLI_ASSOC); 
+    return $records; 
+} else { 
+    // return records that match conditions ... 
+    $sql = "SELECT * FROM users WHERE username='Carter' AND admin=1";
+}  
+
+} 
+
+$conditions = [ 
+'admin' => 1, 
+'username' => 'Carter'
+];
+
+$users = selectAll('users'); 
+dd($users);
